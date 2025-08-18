78313e81add94f56671fecfb2518a5994350a356
Arvind Gautam <iitarvindgautam@gmail.com>
2025-08-18 21:08:29 +0530

    Time: 724 ms (26.78%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,12 +1,8 @@
-
-
-select 
-round(sum(case when a.order_date = a.customer_pref_delivery_date then 1 else 0 end) / count(*) * 100 , 2)
-as immediate_percentage
-from 
-(
-select * ,
-dense_rank() over(partition by customer_id order by order_date) as rnk
+Select 
+    round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
 from Delivery
-) a
-where a.rnk=1
+where (customer_id, order_date) in (
+  Select customer_id, min(order_date) 
+  from Delivery
+  group by customer_id
+);
